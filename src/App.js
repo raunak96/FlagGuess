b99b74ee942283bfd9a1877d83e2676c7f95e260
EC2 Default User <ec2-user@ip-172-31-41-28.us-east-2.compute.internal>
2019-06-13 15:59:53 +0000

    testing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import CountryGame from './CountryGame';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="flag-app">
+        <header className="title-header">
+          <h1 className="title-text">Guess The Flag</h1>
+        </header>
+        <CountryGame />
+      </div>
+    );
+  }
 }
 
 export default App;
