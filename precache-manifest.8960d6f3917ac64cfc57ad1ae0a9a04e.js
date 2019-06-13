c7ccbfe9244da2f635a4706dadd7070b82a371b4
EC2 Default User <ec2-user@ip-172-31-41-28.us-east-2.compute.internal>
2019-06-13 15:58:08 +0000

    Updates

---
+++ precache-manifest.8960d6f3917ac64cfc57ad1ae0a9a04e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75db194e11338a52d773675350c5ab64",
+    "url": "/FlagGuess/index.html"
+  },
+  {
+    "revision": "b459deb0f6e477198f72",
+    "url": "/FlagGuess/static/css/main.522108e7.chunk.css"
+  },
+  {
+    "revision": "16ddd17995a7e984c042",
+    "url": "/FlagGuess/static/js/2.06f23ff5.chunk.js"
+  },
+  {
+    "revision": "b459deb0f6e477198f72",
+    "url": "/FlagGuess/static/js/main.96eee104.chunk.js"
+  },
+  {
+    "revision": "e3f5c4973958ee546c9f",
+    "url": "/FlagGuess/static/js/runtime~main.c5673a6a.js"
+  }
+]);
